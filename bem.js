7f65a82440bda62a460a3d64867526dfe726141f
cs <bartus.csongor@gmail.com>
2015-03-10 23:58:22 +0200

    Blocks and levels done

--- bem.js
+++ bem.js
@@ -1,22 +1,52 @@
 #!/usr/bin/env node
 
 
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+
 
 // Generators
-
-
-var fs = require('fs');
+// -----------------------------------------------------------------------------
 
 // Create a folder
 var makeFolder = function(path) {
-  fs.mkdirSync(path);
-  console.log(path + ' created.');
+  mkdirp(path, function (err) {
+    if (err) throw err;
+    console.log('Dir ok');
+  });
+}
+
+
+// Create a file with content
+var makeFile = function(file, content) {
+  fs.writeFile(file, content, function(err) {
+    if (err) {
+      console.log("Error creating file: " + file);
+    } else {
+      console.log("File ok");
+    }
+  });
+}
+
+
+// Create a block
+var makeBlock = function(path) {
+  splits = path.split('/');
+  block = splits[splits.length - 1];
+  file = path + '/' + block;
+
+  makeFolder(path);
+
+  makeFile(file + '.html.swig', block);
+  makeFile(file + '.scss', "@mixin " + block + " {}");
 }
 
 
 
 
+
 // Command line parser
+// -----------------------------------------------------------------------------
 
 var argv = require('yargs')
   .usage('Usage: $0 <object> path')
@@ -33,6 +63,10 @@ switch (object) {
   case 'level':
   case 'l':
     makeFolder(path);
+    break;
+  case 'block':
+  case 'b':
+    makeBlock(path);
     break;
   default:
     console.log('Wrong BEM object: ' + object);
